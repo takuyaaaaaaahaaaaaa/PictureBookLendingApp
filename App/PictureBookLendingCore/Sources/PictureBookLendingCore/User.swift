074c631d7f0d76a521f2d842ee07f2b79376aa64
takuyaaaaaaahaaaaaa <majikani2011@gmail.com>
2025-05-16 04:08:39 +0900

    add 共通モジュールの作成

---
+++ App/PictureBookLendingCore/Sources/PictureBookLendingCore/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  PictureBookLendingCore
+//
+//  Created by takuya_tominaga on 5/16/25.
+//
+
+public struct User {
+    let name: String
+    public init(name: String) {
+        self.name = name
+    }
+}
