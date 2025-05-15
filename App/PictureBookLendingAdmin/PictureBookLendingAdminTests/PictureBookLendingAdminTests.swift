63e3e912b395bf3a9ac5d06f194967a99d65f182
takuyaaaaaaahaaaaaa <majikani2011@gmail.com>
2025-05-16 03:47:44 +0900

    add workspaceの追加

---
+++ App/PictureBookLendingAdmin/PictureBookLendingAdminTests/PictureBookLendingAdminTests.swift
@@ -0,0 +1,16 @@
+//
+//  PictureBookLendingAdminTests.swift
+//  PictureBookLendingAdminTests
+//
+//  Created by takuya_tominaga on 5/16/25.
+//
+
+import Testing
+
+struct PictureBookLendingAdminTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
